2738a2b78bb4c8cf73996406570e11c8c4977293
momeemt <momiimt@gmail.com>
2021-04-09 21:17:27 +0900

    🚧👍 Add(WIP): libavcodec/qsv

---
+++ src/ffmpeg/libavcodec_qsv.nim
@@ -0,0 +1,19 @@
+when defined(windows):
+  {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avcodec(|.55|.56|.57|.58|.59).dylib".}
+else:
+  {.push importc, dynlib: "libavcodec.so(|.55|.56|.57|.58|.59)".}
+
+type
+  AVQSVContext* = object
+    session: mfxSession
+    iopattern: cint
+    ext_buffers: ptr ptr mfxExtBuffer
+    nb_ext_buffers: cint
+    opaque_alloc: cint
+    nb_opaque_surfaces: cint
+    opaque_surfaces: ptr AVBufferRef
+    opaque_alloc_type: cint
+
+proc av_qsv_alloc_context* (): AVQSVContext
