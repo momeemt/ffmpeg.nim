a3bdcb390bb8c8b0e94ce5c3f4be89332a430994
momeemt <momiimt@gmail.com>
2021-04-09 23:33:58 +0900

    🚧👍  Add(WIP): libavcodec/xvmc

---
+++ src/ffmpeg/libavcodec_xvmc.nim
@@ -0,0 +1,27 @@
+when defined(windows):
+  {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avcodec(|.55|.56|.57|.58|.59).dylib".}
+else:
+  {.push importc, dynlib: "libavcodec.so(|.55|.56|.57|.58|.59)".}
+
+const
+  AV_XVMC_ID* = 0x1DC711C0
+
+type
+  xvmc_pix_fmt* {.deprecated.} = object
+    xvmc_id: cint
+    data_blocks: ptr cshort
+    mv_blocks: ptr XvMCMacroBlock
+    allocated_mv_blocks: cint
+    allocated_data_blocks: cint
+    idct: cint
+    unsigned_intra: cint
+    p_surface: ptr XvMCSurface
+    p_past_surface: ptr XvMCSurface
+    p_future_surface: ptr XvMCSurface
+    picture_structure: cuint
+    flags: cuint
+    start_mv_blocks_num: cint
+    filled_mv_blocks_num: cint
+    next_free_data_block_num: cint
