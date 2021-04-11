6c7fd15b1244e4eb2e566125a8197353762aa9b2
momeemt <momiimt@gmail.com>
2021-04-11 12:18:30 +0900

    👍  Add: libavutil/aes

---
+++ src/ffmpeg/libavutil_aes.nim
@@ -0,0 +1,15 @@
+when defined(windows):
+  {.push importc, dynlib: "avutil-(|55|56|57).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avutil(|.55|.56|.57).dylib".}
+else:
+  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
+
+var av_aes_size*: cint # TODO: extern const int av_aes_size;
+
+type
+  AVAES* = object
+
+proc av_aes_alloc* (): ptr AVAES
+proc av_aes_init* (a: ptr AVAES, key: ptr uint8, key_bits, decrypt: cint): cint
+proc av_aes_crypt* (a: ptr AVAES, dst, src: ptr uint8, count: cint, iv: ptr uint8, decrypt: cint)
