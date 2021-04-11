76fabc6ba683c20011c64626618108e12b267328
momeemt <momiimt@gmail.com>
2021-04-11 13:40:03 +0900

    👍  Add; libavutil/blowfish

---
+++ src/ffmpeg/libavutil_blowfish.nim
@@ -0,0 +1,19 @@
+when defined(windows):
+  {.push importc, dynlib: "avutil-(|55|56|57).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avutil(|.55|.56|.57).dylib".}
+else:
+  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
+
+const
+  AV_BF_ROUNDS* = 16
+
+type
+  AVBlowfish* = object
+    p: array[AV_BF_ROUNDS + 2, uint32]
+    s: array[4, array[256, uint32]]
+
+proc av_blowfish_alloc* (): AVBlowfish
+proc av_blowfish_init* (ctx: ptr AVBlowfish, key: ptr uint8, key_len: cint)
+proc av_blowfish_crypt_ecb* (ctx: ptr AVBlowfish, xl, xr: ptr uint32, decrypt: cint)
+proc av_blowfish_crypt* (ctx: ptr AVBlowfish, dst, src: ptr uint8, count: cint, iv: ptr uint8, decrypt: cint)
