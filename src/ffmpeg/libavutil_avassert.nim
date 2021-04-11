a1c92ff69a9dcb86a43a662fa6d247adad1b7b22
momeemt <momiimt@gmail.com>
2021-04-11 13:11:05 +0900

    👍  Add: libavutil/avassert

---
+++ src/ffmpeg/libavutil_avassert.nim
@@ -0,0 +1,32 @@
+when defined(windows):
+  {.push importc, dynlib: "avutil-(|55|56|57).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avutil(|.55|.56|.57).dylib".}
+else:
+  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
+
+template av_assert0* (cond: untyped): untyped =
+  if not cond:
+    # av_log(nil, AV_LOG_PANIC, "Assertion %s failed at %s:%d\n", AV_STRINGIFY(cond), __FILE__, __LINE__)
+    abort()
+
+proc av_assert0_fpu* ()
+
+when defined(ASSERT_LEVEL):
+  when ASSERT_LEVEL > 0:
+    template av_assert1* (cond: untyped): untyped = av_assert0(cond)
+  else:
+    template av_assert1* (cond: untyped): untyped = 0
+else:
+  template av_assert1* (cond: untyped): untyped = 0
+
+when defined(ASSERT_LEVEL):
+  when ASSERT_LEVEL > 1:
+    template av_assert2* (cond: untyped): untyped = av_assert0(cond)
+    template av_assert2_fpu* (): untyped = av_assert0_fpu()
+  else:
+    template av_assert2* (cond: untyped): untyped = 0
+    template av_assert2_fpu* (): untyped = 0
+else:
+  template av_assert2* (cond: untyped): untyped = 0
+  template av_assert2_fpu* (): untyped = 0
