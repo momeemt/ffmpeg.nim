69c726466f90a91fc1fcc8026be16c843d4bfb46
momeemt <momiimt@gmail.com>
2021-04-11 22:09:59 +0900

    👍  Add: libavutil/ffversion

---
+++ src/ffmpeg/libavutil_ffversion.nim
@@ -0,0 +1,9 @@
+when defined(windows):
+  {.push importc, dynlib: "avutil-(|55|56|57).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avutil(|.55|.56|.57).dylib".}
+else:
+  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
+
+const
+  FFMPEG_VERSION* = "4.3.2"
