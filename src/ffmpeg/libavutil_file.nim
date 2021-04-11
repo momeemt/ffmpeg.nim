8ff09e2b2d608ee5900e5240aac4112d00cf4a1c
momeemt <momiimt@gmail.com>
2021-04-11 22:22:42 +0900

    👍  Add: libavutil/file

---
+++ src/ffmpeg/libavutil_file.nim
@@ -0,0 +1,10 @@
+when defined(windows):
+  {.push importc, dynlib: "avutil-(|55|56|57).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avutil(|.55|.56|.57).dylib".}
+else:
+  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
+
+proc av_file_map* (filename: ptr cchar, bufptr: ptr ptr uint8, size: ptr csize_t, log_offset: cint, log_ctx: pointer): cint # warning
+proc av_file_unmap* (bufptr: ptr uint8, size: csize_t)
+proc av_tempfile* (prefix: ptr cchar, filename: ptr ptr cchar, log_offset: cuint, log_ctx: pointer): cint
