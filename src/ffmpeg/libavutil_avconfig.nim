a043c666cbfa48e1f053dd58efa82c7c5dae80ab
momeemt <momiimt@gmail.com>
2021-04-11 13:12:28 +0900

    👍  Add: libavutil/avconfig

---
+++ src/ffmpeg/libavutil_avconfig.nim
@@ -0,0 +1,10 @@
+when defined(windows):
+  {.push importc, dynlib: "avutil-(|55|56|57).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avutil(|.55|.56|.57).dylib".}
+else:
+  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
+
+const
+  AV_HAVE_BIGENDIAN* = 0
+  AV_HAVE_FAST_UNALIGNED* = 1
