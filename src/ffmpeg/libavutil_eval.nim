b1af800f8d61963e8521029ed12bab66f8b002ca
momeemt <momiimt@gmail.com>
2021-04-11 22:09:09 +0900

    👍  Add: libavutil/eval

---
+++ src/ffmpeg/libavutil_eval.nim
@@ -0,0 +1,17 @@
+when defined(windows):
+  {.push importc, dynlib: "avutil-(|55|56|57).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avutil(|.55|.56|.57).dylib".}
+else:
+  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
+
+type
+  AVExpr* = object
+
+proc av_expr_parse_and_eval* (res: ptr cdouble, s: ptr cchar, const_names: ptr ptr cchar, const_values: ptr cdouble, func1_names: ptr ptr cchar, funcs1: proc (a1: pointer, a2: cdouble): cdouble, func2_names: ptr ptr cchar, funcs2: proc (a1: pointer, a2: cdouble, a3: cdouble): cdouble, opaque: pointer, log_offset: cint, log_ctx: pointer): cint
+proc av_expr_parse* (expr: ptr ptr AVExpr, s: ptr cchar, const_names: ptr ptr cchar, const_values: ptr cdouble, func1_names: ptr ptr cchar, funcs1: proc (a1: pointer, a2: cdouble): cdouble, func2_names: ptr ptr cchar, funcs2: proc (a1: pointer, a2: cdouble, a3: cdouble): cdouble, opaque: pointer, log_offset: cint, log_ctx: pointer): cint
+proc av_expr_eval* (e: ptr AVExpr, const_values: ptr cdouble, opaque: pointer): cdouble
+proc av_expr_count_vars* (e: ptr AVExpr, counter: ptr cuint, size: cint): cint
+proc av_expr_count_func* (e: ptr AVExpr, counter: ptr cuint, size, arg: cint): cint
+proc av_expr_free* (e: ptr AVExpr)
+proc av_strtod* (numstr: ptr cchar, tail: ptr ptr cchar): cdouble
