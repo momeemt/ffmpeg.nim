f9fe6a17bfd068e7c039cbff55db8519f6852bcc
momeemt <momiimt@gmail.com>
2021-04-12 21:48:21 +0900

    👍  Add: libavutil/hwcontext_mediacodec

---
+++ src/ffmpeg/libavutil_hwcontext_mediacodec.nim
@@ -0,0 +1,10 @@
+when defined(windows):
+  {.push importc, dynlib: "avutil-(|55|56|57).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avutil(|.55|.56|.57).dylib".}
+else:
+  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
+
+type
+  AVMediaCodecDeviceContext* = object
+    surface: pointer
