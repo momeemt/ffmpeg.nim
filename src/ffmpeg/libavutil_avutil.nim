d91d985df48e7f648af97ab7c5d66b11fcc6c2fb
momeemt <momiimt@gmail.com>
2021-04-14 01:08:43 +0900

    👍  Update: add pragmma

--- src/ffmpeg/libavutil_avutil.nim
+++ src/ffmpeg/libavutil_avutil.nim
@@ -1,11 +1,33 @@
+from libavutil_rational import AVRational
+
+{.pragma: avutil, importc, header: "<libavutil/avutil.h>".}
+
+type
+  AVMediaType* {.avutil.} = enum
+    AVMEDIA_TYPE_UNKNOWN = -1
+    AVMEDIA_TYPE_VIDEO
+    AVMEDIA_TYPE_AUDIO
+    AVMEDIA_TYPE_DATA
+    AVMEDIA_TYPE_SUBTITLE
+    AVMEDIA_TYPE_ATTACHMENT
+    AVMEDIA_TYPE_NB
+  
+  AVPictureType* {.avutil.} = enum
+    AV_PICTURE_TYPE_NONE = 0
+    AV_PICTURE_TYPE_I
+    AV_PICTURE_TYPE_P
+    AV_PICTURE_TYPE_B
+    AV_PICTURE_TYPE_S
+    AV_PICTURE_TYPE_SI
+    AV_PICTURE_TYPE_SP
+    AV_PICTURE_TYPE_BI
+
 when defined(windows):
   {.push importc, dynlib: "avutil-(|55|56|57).dll".}
 elif defined(macosx):
   {.push importc, dynlib: "avutil(|.55|.56|.57).dylib".}
 else:
   {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
-
-import libavutil_rational
 
 const
   FF_LAMBDA_SHIFT* = 7
@@ -17,27 +39,6 @@ const
   AV_TIME_BASE* = 1000000
   #define AV_TIME_BASE_Q          (AVRational){1, AV_TIME_BASE}
   AV_FOURCC_MAX_STRING_SIZE* = 32
-
-
-type
-  AVMediaType* = enum
-    AVMEDIA_TYPE_UNKNOWN = -1
-    AVMEDIA_TYPE_VIDEO
-    AVMEDIA_TYPE_AUDIO
-    AVMEDIA_TYPE_DATA
-    AVMEDIA_TYPE_SUBTITLE
-    AVMEDIA_TYPE_ATTACHMENT
-    AVMEDIA_TYPE_NB
-  
-  AVPictureType* = enum
-    AV_PICTURE_TYPE_NONE = 0
-    AV_PICTURE_TYPE_I
-    AV_PICTURE_TYPE_P
-    AV_PICTURE_TYPE_B
-    AV_PICTURE_TYPE_S
-    AV_PICTURE_TYPE_SI
-    AV_PICTURE_TYPE_SP
-    AV_PICTURE_TYPE_BI
 
 proc avutil_version* (): cuint
 proc av_version_info* (): ptr cchar
