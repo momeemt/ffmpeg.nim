754af257fa50ccc1a146eb8b24d733b1cae560d3
momeemt <momiimt@gmail.com>
2021-04-14 01:10:10 +0900

    🎉  Complete: libavutil/blowfish

--- src/ffmpeg/libavutil_blowfish.nim
+++ src/ffmpeg/libavutil_blowfish.nim
@@ -5,13 +5,15 @@ elif defined(macosx):
 else:
   {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
 
+{.pragma: blowfish, importc, header: "<libavutil/blowfish.h>".}
+
 const
   AV_BF_ROUNDS* = 16
 
 type
-  AVBlowfish* = object
-    p: array[AV_BF_ROUNDS + 2, uint32]
-    s: array[4, array[256, uint32]]
+  AVBlowfish* {.blowfish.} = object
+    p*: array[AV_BF_ROUNDS + 2, uint32]
+    s*: array[4, array[256, uint32]]
 
 proc av_blowfish_alloc* (): AVBlowfish
 proc av_blowfish_init* (ctx: ptr AVBlowfish, key: ptr uint8, key_len: cint)
