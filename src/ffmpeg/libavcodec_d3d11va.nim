b6e955bb68eb36ce26ee259abf6e1b83a431f567
momeemt <momiimt@gmail.com>
2021-04-13 17:44:52 +0900

    🎉  Complete: libavcodec/d3d11va

--- src/ffmpeg/libavcodec_d3d11va.nim
+++ src/ffmpeg/libavcodec_d3d11va.nim
@@ -1,22 +1,19 @@
-when defined(windows):
-  {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll".}
-elif defined(macosx):
-  {.push importc, dynlib: "avcodec(|.55|.56|.57|.58|.59).dylib".}
-else:
-  {.push importc, dynlib: "libavcodec.so(|.55|.56|.57|.58|.59)".}
+from undefined_symbol import ID3D11VideoDecoder, ID3D11VideoContext, D3D11_VIDEO_DECODER_CONFIG, ID3D11VideoDecoderOutputView, HANDLE
+
+{.pragma: d3d11va, importc, header:"<libavcodec/d3d11va.h>".}
+
+const
+  FF_DXVA2_WORKAROUND_SCALING_LIST_ZIGZAG* = 1
+  FF_DXVA2_WORKAROUND_INTEL_CLEARVIDEO* = 2
 
 #if !defined(_WIN32_WINNT) || _WIN32_WINNT < 0x0602
 #undef _WIN32_WINNT
 #define _WIN32_WINNT 0x0602
 #endif
 
-const
-  FF_DXVA2_WORKAROUND_SCALING_LIST_ZIGZAG* = 1
-  FF_DXVA2_WORKAROUND_INTEL_CLEARVIDEO* = 2
-
 type
   AVD3D11VAContext* = object
-    decoder: ptr ID3D11VideoDecoder
+    decoder*: ptr ID3D11VideoDecoder
     video_context: ptr ID3D11VideoContext
     cfg: ptr D3D11_VIDEO_DECODER_CONFIG
     surface_count: cuint
@@ -24,5 +21,12 @@ type
     workaround: uint64
     report_id: cuint
     context_mutex: HANDLE
+
+when defined(windows):
+  {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avcodec(|.55|.56|.57|.58|.59).dylib".}
+else:
+  {.push importc, dynlib: "libavcodec.so(|.55|.56|.57|.58|.59)".}
 
 proc av_d3d11va_alloc_context* (): AVD3D11VAContext
