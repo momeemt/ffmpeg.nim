625e1ac47c4a2184daec3177516fcbace0cd8a70
momeemt <momiimt@gmail.com>
2021-04-14 01:52:39 +0900

    👍  Update: import libavutil_pixfmt

--- src/ffmpeg/libavutil_hwcontext_videotoolbox.nim
+++ src/ffmpeg/libavutil_hwcontext_videotoolbox.nim
@@ -1,3 +1,5 @@
+from libavutil_pixfmt import AVPixelFormat
+
 when defined(windows):
   {.push importc, dynlib: "avutil-(|55|56|57).dll".}
 elif defined(macosx):
@@ -6,7 +8,6 @@ else:
   {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
 
 #include <VideoToolbox/VideoToolbox.h>
-#include "pixfmt.h"
 
 proc av_map_videotoolbox_format_to_pixfmt* (cv_fmt: cuint): AVPixelFormat
 proc av_map_videotoolbox_format_from_pixfmt* (pix_fmt: AVPixelFormat): cuint
