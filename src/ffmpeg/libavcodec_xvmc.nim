73994096f4fb644c24440bb9f2e9db3eb98fc00e
momeemt <momiimt@gmail.com>
2021-04-13 21:00:34 +0900

    🛠  Fix: publish member

--- src/ffmpeg/libavcodec_xvmc.nim
+++ src/ffmpeg/libavcodec_xvmc.nim
@@ -6,21 +6,21 @@ type
   XvMCSurface* {.xvmcInclude.} = object
 
   xvmc_pix_fmt* {.xvmc, deprecated.} = object
-    xvmc_id: cint
-    data_blocks: ptr cshort
-    mv_blocks: ptr XvMCMacroBlock
-    allocated_mv_blocks: cint
-    allocated_data_blocks: cint
-    idct: cint
-    unsigned_intra: cint
-    p_surface: ptr XvMCSurface
-    p_past_surface: ptr XvMCSurface
-    p_future_surface: ptr XvMCSurface
-    picture_structure: cuint
-    flags: cuint
-    start_mv_blocks_num: cint
-    filled_mv_blocks_num: cint
-    next_free_data_block_num: cint
+    xvmc_id*: cint
+    data_blocks*: ptr cshort
+    mv_blocks*: ptr XvMCMacroBlock
+    allocated_mv_blocks*: cint
+    allocated_data_blocks*: cint
+    idct*: cint
+    unsigned_intra*: cint
+    p_surface*: ptr XvMCSurface
+    p_past_surface*: ptr XvMCSurface
+    p_future_surface*: ptr XvMCSurface
+    picture_structure*: cuint
+    flags*: cuint
+    start_mv_blocks_num*: cint
+    filled_mv_blocks_num*: cint
+    next_free_data_block_num*: cint
 
 when defined(windows):
   {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll".}
