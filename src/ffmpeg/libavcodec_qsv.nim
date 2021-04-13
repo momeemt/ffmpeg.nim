3aab85a257201ba88f43f90a1f960bb42bd00ed9
momeemt <momiimt@gmail.com>
2021-04-13 20:38:22 +0900

    🛠  Fix: publish member

--- src/ffmpeg/libavcodec_qsv.nim
+++ src/ffmpeg/libavcodec_qsv.nim
@@ -3,14 +3,14 @@ from undefined_symbol import mfxSession, mfxExtBuffer
 
 type
   AVQSVContext* = object
-    session: mfxSession
-    iopattern: cint
-    ext_buffers: ptr ptr mfxExtBuffer
-    nb_ext_buffers: cint
-    opaque_alloc: cint
-    nb_opaque_surfaces: cint
-    opaque_surfaces: ptr AVBufferRef
-    opaque_alloc_type: cint
+    session*: mfxSession
+    iopattern*: cint
+    ext_buffers*: ptr ptr mfxExtBuffer
+    nb_ext_buffers*: cint
+    opaque_alloc*: cint
+    nb_opaque_surfaces*: cint
+    opaque_surfaces*: ptr AVBufferRef
+    opaque_alloc_type*: cint
 
 when defined(windows):
   {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll".}
