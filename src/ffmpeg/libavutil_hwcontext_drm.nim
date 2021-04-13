b675cc812a476ad43b49f82261092e628e1e496f
momeemt <momiimt@gmail.com>
2021-04-14 01:57:00 +0900

    🎉  Complete: libavutil/hwcontext_drm

--- src/ffmpeg/libavutil_hwcontext_drm.nim
+++ src/ffmpeg/libavutil_hwcontext_drm.nim
@@ -5,30 +5,32 @@ elif defined(macosx):
 else:
   {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
 
+{.pragma: hwcontext_drm, importc, header: "<libavutil/hwcontext_drm.h>".}
+
 const
   AV_DRM_MAX_PLANES* = 4
 
 type
-  AVDRMObjectDescriptor* = object
-    fg: cint
-    size: csize_t
-    format_modifier: uint64
+  AVDRMObjectDescriptor* {.hwcontext_drm.} = object
+    fg*: cint
+    size*: csize_t
+    format_modifier*: uint64
 
-  AVDRMPlaneDescriptor* = object
-    object_index: cint
-    offset: ptrdiff_t
-    pitch: ptrdiff_t
+  AVDRMPlaneDescriptor* {.hwcontext_drm.} = object
+    object_index*: cint
+    offset*: ByteAddress
+    pitch*: ByteAddress
   
-  AVDRMLayerDescriptor* = object
-    format: cuint
-    nb_planes: cint
-    planes: array[AV_DRM_MAX_PLANES, AVDRMPlaneDescriptor]
+  AVDRMLayerDescriptor* {.hwcontext_drm.} = object
+    format*: cuint
+    nb_planes*: cint
+    planes*: array[AV_DRM_MAX_PLANES, AVDRMPlaneDescriptor]
   
-  AVDRMFrameDescriptor* = object
-    nb_objects: cint
-    objects: array[AV_DRM_MAX_PLANES, AVDRMObjectDescriptor]
-    nb_layers: cint
-    layers: array[AV_DRM_MAX_PLANES, AVDRMLayerDescriptor]
+  AVDRMFrameDescriptor* {.hwcontext_drm.} = object
+    nb_objects*: cint
+    objects*: array[AV_DRM_MAX_PLANES, AVDRMObjectDescriptor]
+    nb_layers*: cint
+    layers*: array[AV_DRM_MAX_PLANES, AVDRMLayerDescriptor]
   
-  AVDRMDeviceContext* = object
-    fd: cint
+  AVDRMDeviceContext* {.hwcontext_drm.} = object
+    fd*: cint
