40c3962dd6b9eef6f16c1ae568b38d97a85bf7dc
momeemt <momiimt@gmail.com>
2021-04-13 17:15:55 +0900

    🎉  Complete: libavcodec/codec_desc

--- src/ffmpeg/libavcodec_codec_desc.nim
+++ src/ffmpeg/libavcodec_codec_desc.nim
@@ -1,21 +1,23 @@
+from libavcodec_codec import AVProfile
+from libavcodec_codec_id import AVCodecID
+from libavutil_avutil import AVMediaType
+
+type
+  AVCodecDescriptor* = object
+    id*: AVCodecID
+    `type`*: AVMediaType
+    name*: cstring
+    long_name*: cstring
+    props*: cint
+    mime_types*: cstringArray
+    profiles*: ptr AVProfile
+
 when defined(windows):
   {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll".}
 elif defined(macosx):
   {.push importc, dynlib: "avcodec(|.55|.56|.57|.58|.59).dylib".}
 else:
   {.push importc, dynlib: "libavcodec.so(|.55|.56|.57|.58|.59)".}
-
-from libavutil_avutil import AVMediaType
-
-type
-  AVCodecDescriptor* = object
-    id: AVCodecID
-    `type`: AVMediaType
-    name: ptr cchar
-    long_name: ptr cchar
-    props: cint
-    mime_types: ptr ptr cchar
-    profiles: ptr AVProfile
   
 const
   AV_CODEC_PROP_INTRA_ONLY* = 1 shl 0
@@ -27,4 +29,4 @@ const
 
 proc avcodec_descriptor_get* (id: AVCodecID): ptr AVCodecDescriptor
 proc avcodec_descriptor_next* (prev: ptr AVCodecDescriptor): ptr AVCodecDescriptor
-proc avcodec_descriptor_get_by_name* (name: ptr cchar): ptr AVCodecDescriptor
+proc avcodec_descriptor_get_by_name* (name: cstring): ptr AVCodecDescriptor
