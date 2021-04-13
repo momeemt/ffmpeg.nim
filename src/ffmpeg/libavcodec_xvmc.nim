722e241ad4c44d656218b06793ea44efa3ef4838
momeemt <momiimt@gmail.com>
2021-04-13 21:00:05 +0900

    🎉  Complete: libavcodec/xvmc

--- src/ffmpeg/libavcodec_xvmc.nim
+++ src/ffmpeg/libavcodec_xvmc.nim
@@ -1,15 +1,11 @@
-when defined(windows):
-  {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll".}
-elif defined(macosx):
-  {.push importc, dynlib: "avcodec(|.55|.56|.57|.58|.59).dylib".}
-else:
-  {.push importc, dynlib: "libavcodec.so(|.55|.56|.57|.58|.59)".}
-
-const
-  AV_XVMC_ID* = 0x1DC711C0
+{.pragma: xvmcInclude, importc, header: "<X11/extensions/XvMC.h>".}
+{.pragma: xvmc, importc, header: "<libavcodec/xvmc.h>".}
 
 type
-  xvmc_pix_fmt* {.deprecated.} = object
+  XvMCMacroBlock* {.xvmcInclude.} = object
+  XvMCSurface* {.xvmcInclude.} = object
+
+  xvmc_pix_fmt* {.xvmc, deprecated.} = object
     xvmc_id: cint
     data_blocks: ptr cshort
     mv_blocks: ptr XvMCMacroBlock
@@ -25,3 +21,13 @@ type
     start_mv_blocks_num: cint
     filled_mv_blocks_num: cint
     next_free_data_block_num: cint
+
+when defined(windows):
+  {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avcodec(|.55|.56|.57|.58|.59).dylib".}
+else:
+  {.push importc, dynlib: "libavcodec.so(|.55|.56|.57|.58|.59)".}
+
+const
+  AV_XVMC_ID* = 0x1DC711C0
