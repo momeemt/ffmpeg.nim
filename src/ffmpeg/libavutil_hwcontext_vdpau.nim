a189a4bf4a5be66e9df1c8b6b780dc4434be6c3f
momeemt <momiimt@gmail.com>
2021-04-14 01:59:02 +0900

    🎉  Complete: libavutil/hwcontext_vdpau

--- src/ffmpeg/libavutil_hwcontext_vdpau.nim
+++ src/ffmpeg/libavutil_hwcontext_vdpau.nim
@@ -1,13 +1,10 @@
-when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll".}
-elif defined(macosx):
-  {.push importc, dynlib: "avutil(|.55|.56|.57).dylib".}
-else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
-
-#include <vdpau/vdpau.h>
+{.pragma: vdpau, importc, header: "<vdpau/vdpau.h>".}
+{.pragma: hwcontext_vdpau, importc, header: "<libavutil/hwcontext_vdpau.h>".}
 
 type
-  AVVDPAUDeviceContext* = object
+  VdpDevice* {.vdpau.} = object
+  VdpGetProcAddress* {.vdpau.} = object
+
+  AVVDPAUDeviceContext* {.hwcontext_vdpau.} = object
     device: VdpDevice
     get_proc_address: ptr VdpGetProcAddress
