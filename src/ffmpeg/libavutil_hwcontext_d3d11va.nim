18f83002ca93d641d6b2a3d52f9768361733a608
momeemt <momiimt@gmail.com>
2021-04-14 02:15:02 +0900

    🎉  CComplete: libavutil/hwcontext_d3d11va

--- src/ffmpeg/libavutil_hwcontext_d3d11va.nim
+++ src/ffmpeg/libavutil_hwcontext_d3d11va.nim
@@ -5,21 +5,30 @@ elif defined(macosx):
 else:
   {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
 
+{.pragma: d3d11va, importc, header: "<d3d11.h>".}
+{.pragma: hwcontext_d3d11va, importc, header: "<libavutil/hwcontext/d3d11va.h>".}
+
 type
-  AVD3D11VADeviceContext* = object
-    device: ptr ID3D11Device
-    device_context: ptr ID3D11DeviceContext
-    video_device: ptr ID3D11VideoDevice
-    video_context: ptr ID3D11VideoContext
-    lock: proc (lock_ctx: pointer)
-    unlock: proc (lock_ctx: pointer)
-    lock_ctx: pointer
+  ID3D11Device* {.d3d11va.} = object
+  ID3D11DeviceContext* {.d3d11va.} = object
+  ID3D11VideoDevice* {.d3d11va.} = object
+  ID3D11VideoContext* {.d3d11va.} = object
+  ID3D11Texture2D* {.d3d11va.} = object
+
+  AVD3D11VADeviceContext* {.hwcontext_d3d11va.} = object
+    device*: ptr ID3D11Device
+    device_context*: ptr ID3D11DeviceContext
+    video_device*: ptr ID3D11VideoDevice
+    video_context*: ptr ID3D11VideoContext
+    lock*: proc (lock_ctx: pointer)
+    unlock*: proc (lock_ctx: pointer)
+    lock_ctx*: pointer
   
-  AVD3D11FrameDescriptor* = object
-    texture: ptr ID3D11Texture2D
-    index: intptr_t
+  AVD3D11FrameDescriptor* {.hwcontext_d3d11va.} = object
+    texture*: ptr ID3D11Texture2D
+    index*: cint #intptr_t
   
-  AVD3D11VAFramesContext* = object
-    texture: ptr ID3D11Texture2D
-    BindFlags: cuint # UINT
-    MiscFlags: cuint # UINT
+  AVD3D11VAFramesContext* {.hwcontext_d3d11va.} = object
+    texture*: ptr ID3D11Texture2D
+    BindFlags*: cuint # UINT
+    MiscFlags*: cuint # UINT
