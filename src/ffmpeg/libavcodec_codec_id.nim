1b0b37164bfca060d2b5473b3770daf2843c2892
momeemt <momiimt@gmail.com>
2021-04-13 17:18:59 +0900

    🎉  Complete: libavcodec/codec_id

--- src/ffmpeg/libavcodec_codec_id.nim
+++ src/ffmpeg/libavcodec_codec_id.nim
@@ -1,14 +1,9 @@
-when defined(windows):
-  {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll".}
-elif defined(macosx):
-  {.push importc, dynlib: "avcodec(|.55|.56|.57|.58|.59).dylib".}
-else:
-  {.push importc, dynlib: "libavcodec.so(|.55|.56|.57|.58|.59)".}
-
 from libavutil_avutil import AVMediaType
 
+{.pragma: codecId, importc, header:"<libavcodec/codec_id.h>".}
+
 type
-  AVCodecID* = enum
+  AVCodecID* {.codecId.} = enum
     AV_CODEC_ID_NONE
     AV_CODEC_ID_MPEG1VIDEO
     AV_CODEC_ID_MPEG2VIDEO
@@ -495,5 +490,12 @@ const
   AV_CODEC_ID_IFF_BYTERUN1* = AVCodecID.AV_CODEC_ID_IFF_ILBM
   AV_CODEC_ID_H265* = AVCodecID.AV_CODEC_ID_HEVC
 
+when defined(windows):
+  {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avcodec(|.55|.56|.57|.58|.59).dylib".}
+else:
+  {.push importc, dynlib: "libavcodec.so(|.55|.56|.57|.58|.59)".}
+
 proc avcodec_get_type* (codec_id: AVCodecID): AVMediaType
-proc avcodec_get_name* (id: AVCodecID): ptr cchar
+proc avcodec_get_name* (id: AVCodecID): cstring
