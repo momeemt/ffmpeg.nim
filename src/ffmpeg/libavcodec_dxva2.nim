843da45d13ccd513506e8f3b2a39e8b27170e1ee
momeemt <momiimt@gmail.com>
2021-04-13 18:06:37 +0900

    🎉  Complete: libavcodec/dxva2

--- src/ffmpeg/libavcodec_dxva2.nim
+++ src/ffmpeg/libavcodec_dxva2.nim
@@ -1,9 +1,4 @@
-when defined(windows):
-  {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll".}
-elif defined(macosx):
-  {.push importc, dynlib: "avcodec(|.55|.56|.57|.58|.59).dylib".}
-else:
-  {.push importc, dynlib: "libavcodec.so(|.55|.56|.57|.58|.59)".}
+from undefined_symbol import IDirectXVideoDecoder, DXVA2_ConfigPictureDecode, LPDIRECT3DSURFACE9
 
 #if !defined(_WIN32_WINNT) || _WIN32_WINNT < 0x0602
 #undef _WIN32_WINNT
@@ -16,9 +11,9 @@ const
 
 type
   dxva_context* = object
-    decoder: ptr IDirectXVideoDecoder
-    cfg: ptr DXVA2_ConfigPictureDecode
-    surface_count: cuint
-    surface: ptr LPDIRECT3DSURFACE9
-    workaround: uint64
-    report_id: cuint
+    decoder*: ptr IDirectXVideoDecoder
+    cfg*: ptr DXVA2_ConfigPictureDecode
+    surface_count*: cuint
+    surface*: ptr LPDIRECT3DSURFACE9
+    workaround*: uint64
+    report_id*: cuint
