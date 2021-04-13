2c178f029a443cf102ac2d333d9a0262ceed1427
momeemt <momiimt@gmail.com>
2021-04-13 20:36:08 +0900

    🎉  Complete: libavcodec/packet

--- src/ffmpeg/libavcodec_packet.nim
+++ src/ffmpeg/libavcodec_packet.nim
@@ -1,12 +1,11 @@
-when defined(windows):
-  {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll".}
-elif defined(macosx):
-  {.push importc, dynlib: "avcodec(|.55|.56|.57|.58|.59).dylib".}
-else:
-  {.push importc, dynlib: "libavcodec.so(|.55|.56|.57|.58|.59)".}
+from libavutil_buffer import AVBufferRef
+from libavutil_dict import AVDictionary
+from libavutil_rational import AVRational
+
+{.pragma: packet, importc, header:"<libavcodec/packet.h>".}
 
 type
-  AVPacketSideDataType* = enum
+  AVPacketSideDataType* {.packet.} = enum
     AV_PKT_DATA_PALETTE
     AV_PKT_DATA_NEW_EXTRADATA
     AV_PKT_DATA_PARAM_CHANGE
@@ -39,28 +38,28 @@ type
     AV_PKT_DATA_DOVI_CONF
     AV_PKT_DATA_NB
   
-  AVPacketSideData* = object
-    data: ptr uint8
-    size: cint
-    `type`: AVPacketSideDataType
+  AVPacketSideData* {.packet.} = object
+    data*: ptr uint8
+    size*: cint
+    `type`*: AVPacketSideDataType
   
-  AVPacket* = object
-    buf: ptr AVBufferRef
-    pts: int64
-    dts: int64
-    data: ptr uint8
-    size: cint
-    stream_index: cint
-    flags: cint
-    side_data: ptr AVPacketSideDataType
-    side_data_elems: cint
-    duration: int64
-    pos: int64
+  AVPacket* {.packet.} = object
+    buf*: ptr AVBufferRef
+    pts*: int64
+    dts*: int64
+    data*: ptr uint8
+    size*: cint
+    stream_index*: cint
+    flags*: cint
+    side_data*: ptr AVPacketSideDataType
+    side_data_elems*: cint
+    duration*: int64
+    pos*: int64
 
     when defined(FF_API_CONVERGENCE_DURATION):
       convergence_duration {.deprecated.} : int64
   
-  AVSideDataParamChangeFlags* = enum
+  AVSideDataParamChangeFlags* {.packet.} = enum
     AV_SIDE_DATA_PARAM_CHANGE_CHANNEL_COUNT  = 0x0001
     AV_SIDE_DATA_PARAM_CHANGE_CHANNEL_LAYOUT = 0x0002
     AV_SIDE_DATA_PARAM_CHANGE_SAMPLE_RATE    = 0x0004
@@ -74,6 +73,13 @@ const
   AV_PKT_FLAG_TRUSTED* = 0x0008
   AV_PKT_FLAG_DISPOSABLE* = 0x0010
 
+when defined(windows):
+  {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avcodec(|.55|.56|.57|.58|.59).dylib".}
+else:
+  {.push importc, dynlib: "libavcodec.so(|.55|.56|.57|.58|.59)".}
+
 proc av_packet_alloc* (): ptr AVPacket
 proc av_packet_clone* (src: ptr AVPacket): ptr AVPacket
 proc av_packet_free* (pkt: ptr ptr AVPacket)
@@ -86,7 +92,7 @@ proc av_packet_new_side_data* (pkt: ptr AVPacket, `type`: AVPacketSideDataType, 
 proc av_packet_add_side_data* (pkt: ptr AVPacket, `type`: AVPacketSideDataType, data: ptr uint8, size: csize_t): cint
 proc av_packet_shrink_side_data* (pkt: ptr AVPacket, `type`: AVPacketSideDataType, size: cint): cint
 proc av_packet_get_side_data* (pkt: ptr AVPacket, `type`: AVPacketSideDataType, size: ptr cint): ptr uint8
-proc av_packet_side_data_name* (`type`: AVPacketSideDataType): ptr cchar
+proc av_packet_side_data_name* (`type`: AVPacketSideDataType): cstring
 proc av_packet_pack_dictionary* (dict: ptr AVDictionary, size: ptr cint): ptr uint8
 proc av_packet_unpack_dictionary* (data: ptr uint8, size: cint, dict: ptr ptr AVDictionary): cint
 proc av_packet_free_side_data* (pkt: ptr AVPacket)
