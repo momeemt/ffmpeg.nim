0de0755fa7ee961b57d04bdef10487155f291a0f
momeemt <momiimt@gmail.com>
2021-04-14 02:18:35 +0900

    🎉  Complete: libavutil/hwcontext_dxva2

--- src/ffmpeg/libavutil_hwcontext_dxva2.nim
+++ src/ffmpeg/libavutil_hwcontext_dxva2.nim
@@ -1,16 +1,19 @@
-when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll".}
-elif defined(macosx):
-  {.push importc, dynlib: "avutil(|.55|.56|.57).dylib".}
-else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
+{.pragma: d3d9, header: "<d3d9.h>".}
+{.pragma: dxva2, header: "<dxva2api.h>".}
+{.pragma: dxva2Include, importc, d3d9, dxva2.}
+{.pragma: hwcontext_dxva2, importc, header: "<libavutil/hwcontext_dxva2.h>".}
 
 type
-  AVDXVA2DeviceContext* = object
-    devmgr: ptr IDirect3DDeviceManager9
+  IDirect3DDeviceManager9* {.dxva2Include.} = object
+  DWORD* {.dxva2Include.} = object
+  IDirect3DSurface9* {.dxva2Include.} = object
+  IDirectXVideoDecoder* {.dxva2Include.} = object
+
+  AVDXVA2DeviceContext* {.hwcontext_dxva2.} = object
+    devmgr*: ptr IDirect3DDeviceManager9
   
-  AVDXVA2FramesContext* = object
-    surface_type: DWORD
-    surfaces: ptr ptr IDirect3DSurface9
-    nb_surfaces: cint
-    decoder_to_release: ptr IDirectXVideoDecoder
+  AVDXVA2FramesContext* {.hwcontext_dxva2.} = object
+    surface_type*: DWORD
+    surfaces*: ptr ptr IDirect3DSurface9
+    nb_surfaces*: cint
+    decoder_to_release*: ptr IDirectXVideoDecoder
