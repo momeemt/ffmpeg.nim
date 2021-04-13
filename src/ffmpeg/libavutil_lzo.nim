5a09061cc388a490a7fd02deda90473c845d69cc
momeemt <momiimt@gmail.com>
2021-04-14 03:18:00 +0900

    👍  Add: libavutil/lzo

---
+++ src/ffmpeg/libavutil_lzo.nim
@@ -0,0 +1,16 @@
+when defined(windows):
+  {.push importc, dynlib: "avutil-(|55|56|57).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avutil(|.55|.56|.57).dylib".}
+else:
+  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
+
+const
+  AV_LZO_INPUT_DEPLETED* = 1
+  AV_LZO_OUTPUT_FULL* = 2
+  AV_LZO_INVALID_BACKPTR* = 4
+  AV_LZO_ERROR* = 8
+  AV_LZO_INPUT_PADDING* = 8
+  AV_LZO_OUTPUT_PADDING* = 12
+
+proc av_lzo1x_decode* (`out`: pointer, outlen: ptr cint, `in`: pointer, inlen: ptr cint): cint
