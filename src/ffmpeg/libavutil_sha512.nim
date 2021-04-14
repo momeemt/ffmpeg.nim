46bf1077da4e7fe0363c4db0e1afe21041265b2b
momeemt <momiimt@gmail.com>
2021-04-14 12:23:31 +0900

    👍  Add: libavutil/sha512

---
+++ src/ffmpeg/libavutil_sha512.nim
@@ -0,0 +1,22 @@
+{.pragma: sha512, importc, header: "<libavutil/sha512.h>".}
+
+type
+  AVSHA512* {.sha512.} = object
+
+when defined(windows):
+  {.push importc, dynlib: "avutil-(|55|56|57).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avutil(|.55|.56|.57).dylib".}
+else:
+  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
+
+var av_sha512_size*: cint
+
+proc av_sha512_alloc* (): ptr AVSHA512
+proc av_sha512_init* (context: ptr AVSHA512, bits: cint): cint
+proc av_sha512_final* (context: ptr AVSHA512, digest: ptr uint8)
+
+when defined(FF_API_CRYPTO_SIZE_T):
+  proc av_sha512_update* (context: ptr AVSHA512, data: ptr uint8, len: cuint)
+else:
+  proc av_sha512_update* (context: ptr AVSHA512, data: ptr uint8, len: csize_t)
