6838b1fb2454e13c3bb84e25a5af1161c1766045
momeemt <momiimt@gmail.com>
2021-04-14 21:27:17 +0900

    👍  Add: libpostproc_version

---
+++ src/ffmpeg/libpostproc_version.nim
@@ -0,0 +1,17 @@
+from libavutil_version import AV_VERSION_INT, AV_VERSION
+
+when defined(windows):
+  {.push importc, dynlib: "postproc(|-53|-54|-55|-56|-57).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "postproc(|.53|.54|.55|.56|.57).dylib".}
+else:
+  {.push importc, dynlib: "libpostproc.so(|.53|.54|.55|.56|.57)".}
+
+const
+  LIBPOSTPROC_VERSION_MAJOR* = 55
+  LIBPOSTPROC_VERSION_MINOR* = 7
+  LIBPOSTPROC_VERSION_MICRO* = 100
+  LIBPOSTPROC_VERSION_INT* = AV_VERSION_INT(LIBPOSTPROC_VERSION_MAJOR, LIBPOSTPROC_VERSION_MINOR, LIBPOSTPROC_VERSION_MICRO)
+  LIBPOSTPROC_VERSION* = AV_VERSION(LIBPOSTPROC_VERSION_MAJOR, LIBPOSTPROC_VERSION_MINOR, LIBPOSTPROC_VERSION_MICRO)
+  LIBPOSTPROC_BUILD* = LIBPOSTPROC_VERSION_INT
+  LIBPOSTPROC_IDENT* = "postproc"
