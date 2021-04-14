dfdcabba513a263c86fcd61416ab4539ebe828b7
momeemt <momiimt@gmail.com>
2021-04-14 12:03:34 +0900

    👍  Add: libavutil/random_seed

---
+++ src/libavutil_random_seed.nim
@@ -0,0 +1,8 @@
+when defined(windows):
+  {.push importc, dynlib: "avutil-(|55|56|57).dll".}
+elif defined(macosx):
+  {.push importc, dynlib: "avutil(|.55|.56|.57).dylib".}
+else:
+  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
+
+proc av_get_random_seed* (): uint32
