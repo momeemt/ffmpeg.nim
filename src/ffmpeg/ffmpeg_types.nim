16a82036d9415407a661728dec23aa3832f5eab6
momeemt <momiimt@gmail.com>
2021-04-16 20:48:59 +0900

    ♻️  Refactoring: libavutil/hwcontext_drm

--- src/ffmpeg/ffmpeg_types.nim
+++ src/ffmpeg/ffmpeg_types.nim
@@ -42,6 +42,7 @@
 {.pragma: hmac, importc, header: "<libavutil/hmac.h>".}
 {.pragma: cuda, importc, header: "<cuda.h>".}
 {.pragma: hwcontext_cuda, importc, header: "<libavutil/hwcontext_cuda.h>".}
+{.pragma: hwcontext_drm, importc, header: "<libavutil/hwcontext_drm.h>".}
 
 type
   AVDiscard* {.avcodec.} = enum
@@ -2146,3 +2147,27 @@ type
     cuda_ctx*: CUcontext
     stream*: CUstream
     internal*: ptr AVCUDADeviceContextInternal
+  
+  AVDRMObjectDescriptor* {.hwcontext_drm.} = object
+    fg*: cint
+    size*: csize_t
+    format_modifier*: uint64
+
+  AVDRMPlaneDescriptor* {.hwcontext_drm.} = object
+    object_index*: cint
+    offset*: ByteAddress
+    pitch*: ByteAddress
+  
+  AVDRMLayerDescriptor* {.hwcontext_drm.} = object
+    format*: cuint
+    nb_planes*: cint
+    planes*: array[AV_DRM_MAX_PLANES, AVDRMPlaneDescriptor]
+  
+  AVDRMFrameDescriptor* {.hwcontext_drm.} = object
+    nb_objects*: cint
+    objects*: array[AV_DRM_MAX_PLANES, AVDRMObjectDescriptor]
+    nb_layers*: cint
+    layers*: array[AV_DRM_MAX_PLANES, AVDRMLayerDescriptor]
+  
+  AVDRMDeviceContext* {.hwcontext_drm.} = object
+    fd*: cint
