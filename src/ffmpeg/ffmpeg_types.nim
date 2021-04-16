d4ab2e619292a58302a04912b7e6e6cc87638290
momeemt <momiimt@gmail.com>
2021-04-16 20:53:04 +0900

    ♻️  Refactoring: libavutil/hwcontext_vulkan

--- src/ffmpeg/ffmpeg_types.nim
+++ src/ffmpeg/ffmpeg_types.nim
@@ -51,6 +51,8 @@ else:
   {.pragma: hwcontext_opencl_include, importc, header: "<CL/cl.h>".}
 
 {.pragma: hwcontext_opencl, importc, header: "<libavutil/hwcontext_opencl.h>".}
+{.pragma: vulkan, importc, header: "<vulkan/vulkan.h>".}
+{.pragma: hwcontext_vulkan, importc, header: "<libavutil/hwcontext_vulkan.h>".}
 
 type
   AVDiscard* {.avcodec.} = enum
@@ -2189,13 +2191,64 @@ type
   cl_command_queue* {.hwcontext_opencl_include.} = object
 
   AVOpenCLFrameDescriptor* {.hwcontext_opencl.} = object
-    nb_planes: cint
-    planes: array[AV_NUM_DATA_POINTERS, cl_mem]
+    nb_planes*: cint
+    planes*: array[AV_NUM_DATA_POINTERS, cl_mem]
   
   AVOpenCLDeviceContext* {.hwcontext_opencl.} = object
-    device_id: cl_device_id
-    context: cl_context
-    command_queue: cl_command_queue
+    device_id*: cl_device_id
+    context*: cl_context
+    command_queue*: cl_command_queue
   
   AVOpenCLFramesContext* {.hwcontext_opencl.} = object
-    command_queue: cl_command_queue
+    command_queue*: cl_command_queue
+  
+  VkAllocationCallbacks* {.vulkan.} = object
+  VkInstance* {.vulkan.} = object
+  VkPhysicalDevice* {.vulkan.} = object
+  VkDevice* {.vulkan.} = object
+  VkPhysicalDeviceFeatures2* {.vulkan.} = object
+  VkImageTiling* {.vulkan.} = object
+  VkImageUsageFlagBits* {.vulkan.} = object
+  VkImage* {.vulkan.} = object
+  VkDeviceMemory* {.vulkan.} = object
+  VkMemoryPropertyFlagBits* {.vulkan.} = object
+  VkAccessFlagBits* {.vulkan.} = object
+  VkImageLayout* {.vulkan.} = object
+  VkSemaphore* {.vulkan.} = object
+  VkFormat* {.vulkan.} = object
+  
+  AVVkFrameInternal* {.hwcontext_vulkan.} = object
+
+  AVVulkanDeviceContext* {.hwcontext_vulkan.} = object
+    alloc*: ptr VkAllocationCallbacks
+    inst*: VkInstance
+    phys_dev*: VkPhysicalDevice
+    act_dev*: VkDevice
+    queue_family_index*: cuint
+    nb_graphics_queues*: cuint
+    queue_family_tx_index*: cuint
+    nb_tx_queues*: cuint
+    queue_family_comp_index*: cuint
+    nb_comp_queues*: cuint
+    enabled_inst_extensions*: cstring
+    nb_enabled_inst_extensions*: cint
+    enabled_dev_extensions*: cstring
+    nb_enabled_dev_extensions*: cint
+    device_features*: VkPhysicalDeviceFeatures2
+  
+  AVVulkanFramesContext* {.hwcontext_vulkan.} = object
+    tiling*: VkImageTiling
+    usage*: VkImageUsageFlagBits
+    create_pnext*: pointer
+    alloc_pnext*: array[AV_NUM_DATA_POINTERS, pointer]
+  
+  AVVkFrame* {.hwcontext_vulkan.} = object
+    img*: array[AV_NUM_DATA_POINTERS, VkImage]
+    tiling*: VkImageTiling
+    mem*: array[AV_NUM_DATA_POINTERS, VkDeviceMemory]
+    size*: array[AV_NUM_DATA_POINTERS, csize_t]
+    flags*: VkMemoryPropertyFlagBits
+    access*: array[AV_NUM_DATA_POINTERS, VkAccessFlagBits]
+    layout*: array[AV_NUM_DATA_POINTERS, VkImageLayout]
+    sem*: array[AV_NUM_DATA_POINTERS, VkSemaphore]
+    internal*: ptr AVVkFrameInternal
--- src/ffmpeg/libavutil_hwcontext_vulkan.nim
+++ src/ffmpeg/libavutil_hwcontext_vulkan.nim
@@ -1,5 +1,4 @@
-from libavutil_frame import AV_NUM_DATA_POINTERS
-from libavutil_pixfmt import AVPixelFormat
+import ffmpeg_types
 
 when defined(windows):
   {.push importc, dynlib: "avutil-(|55|56|57).dll".}
@@ -7,61 +6,6 @@ elif defined(macosx):
   {.push importc, dynlib: "avutil(|.55|.56|.57).dylib".}
 else:
   {.push importc, dynlib: "libavutil.so(|.55|.56|.57)".}
-
-{.pragma: vulkan, importc, header: "<vulkan/vulkan.h>".}
-{.pragma: hwcontext_vulkan, importc, header: "<libavutil/hwcontext_vulkan.h>".}
-
-type
-  VkAllocationCallbacks* {.vulkan.} = object
-  VkInstance* {.vulkan.} = object
-  VkPhysicalDevice* {.vulkan.} = object
-  VkDevice* {.vulkan.} = object
-  VkPhysicalDeviceFeatures2* {.vulkan.} = object
-  VkImageTiling* {.vulkan.} = object
-  VkImageUsageFlagBits* {.vulkan.} = object
-  VkImage* {.vulkan.} = object
-  VkDeviceMemory* {.vulkan.} = object
-  VkMemoryPropertyFlagBits* {.vulkan.} = object
-  VkAccessFlagBits* {.vulkan.} = object
-  VkImageLayout* {.vulkan.} = object
-  VkSemaphore* {.vulkan.} = object
-  VkFormat* {.vulkan.} = object
-  
-  AVVkFrameInternal* {.hwcontext_vulkan.} = object
-
-  AVVulkanDeviceContext* {.hwcontext_vulkan.} = object
-    alloc*: ptr VkAllocationCallbacks
-    inst*: VkInstance
-    phys_dev*: VkPhysicalDevice
-    act_dev*: VkDevice
-    queue_family_index*: cuint
-    nb_graphics_queues*: cuint
-    queue_family_tx_index*: cuint
-    nb_tx_queues*: cuint
-    queue_family_comp_index*: cuint
-    nb_comp_queues*: cuint
-    enabled_inst_extensions*: cstring
-    nb_enabled_inst_extensions*: cint
-    enabled_dev_extensions*: cstring
-    nb_enabled_dev_extensions*: cint
-    device_features*: VkPhysicalDeviceFeatures2
-  
-  AVVulkanFramesContext* {.hwcontext_vulkan.} = object
-    tiling*: VkImageTiling
-    usage*: VkImageUsageFlagBits
-    create_pnext*: pointer
-    alloc_pnext*: array[AV_NUM_DATA_POINTERS, pointer]
-  
-  AVVkFrame* {.hwcontext_vulkan.} = object
-    img*: array[AV_NUM_DATA_POINTERS, VkImage]
-    tiling*: VkImageTiling
-    mem*: array[AV_NUM_DATA_POINTERS, VkDeviceMemory]
-    size*: array[AV_NUM_DATA_POINTERS, csize_t]
-    flags*: VkMemoryPropertyFlagBits
-    access*: array[AV_NUM_DATA_POINTERS, VkAccessFlagBits]
-    layout*: array[AV_NUM_DATA_POINTERS, VkImageLayout]
-    sem*: array[AV_NUM_DATA_POINTERS, VkSemaphore]
-    internal*: ptr AVVkFrameInternal
 
 proc av_vk_frame_alloc* (): AVVkFrame
 proc av_vkfmt_from_pixfmt* (p: AVPixelFormat): ptr VkFormat
