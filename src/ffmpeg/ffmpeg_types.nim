d80af9af32f0b989a426dbf6cf2195427f39121f
momeemt <momiimt@gmail.com>
2021-04-16 20:45:31 +0900

    ♻️  Refactoring: libavutil/hdr_dynamix_metadata

--- src/ffmpeg/ffmpeg_types.nim
+++ src/ffmpeg/ffmpeg_types.nim
@@ -38,6 +38,7 @@
 {.pragma: fifo, importc, header: "<libavutil/fifo.h>".}
 {.pragma: frame, importc, header: "<libavutil/frame.h>".}
 {.pragma: hash, importc, header: "<libavutil/hash.h>".}
+{.pragma: hdr_dynamic_metadata, importc, header: "<libavutil/hdr_dynamic_metadata.h>".}
 
 type
   AVDiscard* {.avcodec.} = enum
@@ -2075,3 +2076,51 @@ type
       qp_table_buf {.deprecated.}: ptr AVBufferRef
   
   AVHashContext* {.hash.} = object
+
+  AVHDRPlusOverlapProcessOption* {.hdr_dynamic_metadata.} = enum
+    AV_HDR_PLUS_OVERLAP_PROCESS_WEIGHTED_AVERAGING = 0
+    AV_HDR_PLUS_OVERLAP_PROCESS_LAYERING = 1
+  
+  AVHDRPlusPercentile* {.hdr_dynamic_metadata.} = object
+    percentage*: uint8
+    percentile*: AVRational
+  
+  AVHDRPlusColorTransformParams* {.hdr_dynamic_metadata.} = object
+    window_upper_left_corner_x*: AVRational
+    window_upper_left_corner_y*: AVRational
+    window_lower_right_corner_x*: AVRational
+    window_lower_right_corner_y*: AVRational
+    center_of_ellipse_x*: uint16
+    center_of_ellipse_y*: uint16
+    rotation_angle*: uint8
+    semimajor_axis_internal_ellipse*: uint16
+    semimajor_axis_external_ellipse*: uint16
+    semiminor_axis_external_ellipse*: uint16
+    overlap_process_option*: AVHDRPlusOverlapProcessOption
+    maxscl*: array[3, AVRational]
+    average_maxrgb*: AVRational
+    num_distribution_maxrgb_percentiles*: uint8
+    distribution_maxrgb*: array[15, AVHDRPlusPercentile]
+    fraction_bright_pixels*: AVRational
+    tone_mapping_flag*: uint8
+    knee_point_x*: AVRational
+    knee_point_y*: AVRational
+    num_bezier_curve_anchors*: uint8
+    bezier_curve_anchors*: array[15, AVRational]
+    color_saturation_mapping_flag*: uint8
+    color_saturation_weight*: AVRational
+  
+  AVDynamicHDRPlus* {.hdr_dynamic_metadata.} = object
+    itu_t_t35_country_code*: uint8
+    application_version*: uint8
+    num_windows*: uint8
+    params*: array[3, AVHDRPlusColorTransformParams]
+    targeted_system_display_maximum_luminance*: AVRational
+    targeted_system_display_actual_peak_luminance_flag*: uint8
+    num_rows_targeted_system_display_actual_peak_luminance*: uint8
+    num_cols_targeted_system_display_actual_peak_luminance*: uint8
+    targeted_system_display_actual_peak_luminance*: array[25, array[25, AVRational]]
+    mastering_display_actual_peak_luminance_flag*: uint8
+    num_rows_mastering_display_actual_peak_luminance*: uint8
+    num_cols_mastering_display_actual_peak_luminance*: uint8
+    mastering_display_actual_peak_luminance*: array[25, array[25, AVRational]]
