68c35eccff96671fd093765017f3dedb804523b0
momeemt <momiimt@gmail.com>
2021-04-16 20:57:12 +0900

    ♻️  Refactoring: libavutil_intreadwrite

--- src/ffmpeg/ffmpeg_types.nim
+++ src/ffmpeg/ffmpeg_types.nim
@@ -55,6 +55,7 @@ else:
 {.pragma: hwcontext_vulkan, importc, header: "<libavutil/hwcontext_vulkan.h>".}
 {.pragma: hwcontext, importc, header: "<libavutil/hwcontext.h>".}
 {.pragma: intfloat, importc, header: "<libavutil/intfloat.h>".}
+{.pragma: intreadwrite, importc, header: "<libavutil/intreadwrite.h>".}
 
 type
   AVDiscard* {.avcodec.} = enum
@@ -2315,3 +2316,21 @@ type
   av_intfloat64* {.intfloat, bycopy, union.} = object
     i*: cuint
     f*: cdouble
+  
+  av_alias64* {.intreadwrite, union.} = object
+    u64*: uint64
+    u32*: array[2, uint32]
+    u16*: array[4, uint16]
+    u8*: array[8, uint8]
+    f64*: cdouble
+    f32*: array[2, cfloat]
+  
+  av_alias32* {.intreadwrite, union.} = object
+    u32*: uint32
+    u16*: array[2, uint16]
+    u8*: array[4, uint8]
+    f32*: cfloat
+  
+  av_alias16* {.intreadwrite, union.} = object
+    u16*: uint16
+    u8*: array[2, uint8]
