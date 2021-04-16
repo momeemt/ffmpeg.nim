7fd0f2a13857063023e922bbfcdd5ae62dafc269
momeemt <momiimt@gmail.com>
2021-04-16 20:51:27 +0900

    ♻️  Refactoring: libavutil/hwcontext_opencl

--- src/ffmpeg/ffmpeg_types.nim
+++ src/ffmpeg/ffmpeg_types.nim
@@ -45,6 +45,13 @@
 {.pragma: hwcontext_drm, importc, header: "<libavutil/hwcontext_drm.h>".}
 {.pragma: hwcontext_mediacodec, importc, header: "libavutil/hwcontext_mediacodec.h".}
 
+when defined(macosx):
+  {.pragma: hwcontext_opencl_include, importc, header: "<OpenCL/cl.h>".}
+else:
+  {.pragma: hwcontext_opencl_include, importc, header: "<CL/cl.h>".}
+
+{.pragma: hwcontext_opencl, importc, header: "<libavutil/hwcontext_opencl.h>".}
+
 type
   AVDiscard* {.avcodec.} = enum
     AVDISCARD_NONE = -16
@@ -2175,3 +2182,20 @@ type
   
   AVMediaCodecDeviceContext* {.hwcontext_mediacodec.} = object
     surface*: pointer
+  
+  cl_mem* {.hwcontext_opencl_include.} = object
+  cl_device_id* {.hwcontext_opencl_include.} = object
+  cl_context* {.hwcontext_opencl_include.} = object
+  cl_command_queue* {.hwcontext_opencl_include.} = object
+
+  AVOpenCLFrameDescriptor* {.hwcontext_opencl.} = object
+    nb_planes: cint
+    planes: array[AV_NUM_DATA_POINTERS, cl_mem]
+  
+  AVOpenCLDeviceContext* {.hwcontext_opencl.} = object
+    device_id: cl_device_id
+    context: cl_context
+    command_queue: cl_command_queue
+  
+  AVOpenCLFramesContext* {.hwcontext_opencl.} = object
+    command_queue: cl_command_queue
