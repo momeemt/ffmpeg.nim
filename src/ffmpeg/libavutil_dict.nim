30d83407087f1e9594ce23fc44b5434b4300de27
momeemt <momiimt@gmail.com>
2021-05-04 21:17:15 +0900

    🛠  Fix: cchar -> cstring

--- src/ffmpeg/libavutil_dict.nim
+++ src/ffmpeg/libavutil_dict.nim
@@ -16,11 +16,11 @@ const
   AV_DICT_APPEND* = 32
   AV_DICT_MULTIKEY* = 64
 
-proc av_dict_get* (m: ptr AVDictionary, key: ptr cchar, prev: ptr AVDictionaryEntry, flags: cint): ptr AVDictionaryEntry
+proc av_dict_get* (m: ptr AVDictionary, key: cstring, prev: ptr AVDictionaryEntry, flags: cint): ptr AVDictionaryEntry
 proc av_dict_count* (m: ptr AVDictionary): cint
-proc av_dict_set* (pm: ptr ptr AVDictionary, key, value: ptr cchar, flags: cint): cint
-proc av_dict_set_int* (pm: ptr ptr AVDictionary, key: ptr cchar, value: int64, flags: cint): cint
-proc av_dict_parse_string* (pm: ptr ptr AVDictionary, str, key_val_sep, pairs_sep: cchar, flags: cint): cint
+proc av_dict_set* (pm: ptr ptr AVDictionary, key, value: cstring, flags: cint): cint
+proc av_dict_set_int* (pm: ptr ptr AVDictionary, key: cstring, value: int64, flags: cint): cint
+proc av_dict_parse_string* (pm: ptr ptr AVDictionary, str, key_val_sep, pairs_sep: cstring, flags: cint): cint
 proc av_dict_copy* (dst: ptr ptr AVDictionary, src: ptr AVDictionary, flags: cint): cint
 proc av_dict_free* (m: ptr ptr AVDictionary)
-proc av_dict_get_string* (m: ptr AVDictionary, buffer: ptr ptr cchar, key_val_sep, pairs_sep: cchar)
+proc av_dict_get_string* (m: ptr AVDictionary, buffer: cstringArray, key_val_sep, pairs_sep: cchar)
