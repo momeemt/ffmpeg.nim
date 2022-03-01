c879894be25ed40cbd9c02c6530332920554f6ae
Mutsuha Asada <momiimt@gmail.com>
2022-03-01 23:18:39 +0900

    👍 mediacodec to support FFmpeg5.0

--- src/libavcodec/mediacodec.nim
+++ src/libavcodec/mediacodec.nim
@@ -1,11 +1,11 @@
-import ffmpeg_types
+from ../types import AVMediaCodecContext, AVCodecContext
 
 when defined(windows):
-  {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll", cdecl.}
+  {.push importc, dynlib: "avcodec(|-58|-59|-60|-61|-62).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavcodec(|.55|.56|.57|.58|.59).dylib", cdecl.}
+  {.push importc, dynlib: "libavcodec(|-58|-59|-60|-61|-62).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavcodec.so(|.55|.56|.57|.58|.59)", cdecl.}
+  {.push importc, dynlib: "libavcodec.so(|-58|-59|-60|-61|-62)", cdecl.}
 
 proc av_mediacodec_alloc_context* (): AVMediaCodecContext
 proc av_mediacodec_default_init* (avctx: ptr AVCodecContext, ctx: ptr AVMediaCodecContext, surface: pointer): cint
