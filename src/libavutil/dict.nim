112e2309b24dda46f3883399a48dad509cb00952
Mutsuha Asada <momiimt@gmail.com>
2022-03-01 22:04:09 +0900

    👍 dict to support ffmpeg5.0

---
+++ src/libavutil/dict.nim
@@ -0,0 +1,33 @@
+{.pragma: dict, importc, header: "<libavutil/dict.h>".}
+
+type
+  AVDictionaryEntry* {.dict, bycopy.} = object
+    key*: cstring
+    value*: cstring
+  
+  AVDictionary* {.dict.} = object
+
+when defined(windows):
+  {.push importc, dynlib: "avcodec(|-58|-59|-60|-61|-62).dll", cdecl.}
+elif defined(macosx):
+  {.push importc, dynlib: "libavcodec(|-58|-59|-60|-61|-62).dylib", cdecl.}
+else:
+  {.push importc, dynlib: "libavcodec.so(|-58|-59|-60|-61|-62)", cdecl.}
+
+const
+  AV_DICT_MATCH_CASE* = 1
+  AV_DICT_IGNORE_SUFFIX* = 2
+  AV_DICT_DONT_STRDUP_KEY* = 4
+  AV_DICT_DONT_STRDUP_VAL* = 8
+  AV_DICT_DONT_OVERWRITE* = 16
+  AV_DICT_APPEND* = 32
+  AV_DICT_MULTIKEY* = 64
+
+proc av_dict_get* (m: ptr AVDictionary, key: cstring, prev: ptr AVDictionaryEntry, flags: cint): ptr AVDictionaryEntry
+proc av_dict_count* (m: ptr AVDictionary): cint
+proc av_dict_set* (pm: ptr ptr AVDictionary, key, value: cstring, flags: cint): cint
+proc av_dict_set_int* (pm: ptr ptr AVDictionary, key: cstring, value: int64, flags: cint): cint
+proc av_dict_parse_string* (pm: ptr ptr AVDictionary, str, key_val_sep, pairs_sep: cstring, flags: cint): cint
+proc av_dict_copy* (dst: ptr ptr AVDictionary, src: ptr AVDictionary, flags: cint): cint
+proc av_dict_free* (m: ptr ptr AVDictionary)
+proc av_dict_get_string* (m: ptr AVDictionary, buffer: cstringArray, key_val_sep, pairs_sep: cchar)
