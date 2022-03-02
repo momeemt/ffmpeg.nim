592241ddcec7d3f2918aad031133ffe1871e8404
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 18:21:25 +0900

    👍 hwcontext_vaapi to support FFmpeg 5.0

--- src/libavutil/hwcontext_vaapi.nim
+++ src/libavutil/hwcontext_vaapi.nim
@@ -1,9 +1,9 @@
 when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll", cdecl.}
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavutil(|.55|.56|.57).dylib", cdecl.}
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)", cdecl.}
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
 
 {.pragma: va, importc, header: "<va/va.h>".}
 {.pragma: hwcontext_vaapi, importc, header: "<libavutil/hwcontext_vaapi.h>".}
