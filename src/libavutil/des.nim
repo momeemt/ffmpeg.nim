21fc4b392dac2046c255b8dc4b0514505fb0f9e4
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 16:26:07 +0900

    👍 des to support FFmpeg 5.0

--- src/libavutil/des.nim
+++ src/libavutil/des.nim
@@ -1,11 +1,11 @@
-import ffmpeg_types
+from ../types import AVDES
 
 when defined(windows):
-  {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll", cdecl.}
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavcodec(|.55|.56|.57|.58|.59).dylib", cdecl.}
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavcodec.so(|.55|.56|.57|.58|.59)", cdecl.}
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
 
 proc av_des_alloc* (): ptr AVDES
 proc av_des_init* (d: ptr AVDES, key: ptr uint8, key_bits, decrypt: cint): cint
