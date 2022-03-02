afaeae861add594ab4180d43cf2a8742ec0ee585
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 19:43:55 +0900

    👍 intfloat to support FFmpeg 5.0

--- src/libavutil/intfloat.nim
+++ src/libavutil/intfloat.nim
@@ -1,11 +1,11 @@
-import ffmpeg_types
+from ../types import av_intfloat32, av_intfloat64
 
 when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll", cdecl.}
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavutil(|.55|.56|.57).dylib", cdecl.}
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)", cdecl.}
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
 
 proc av_int2float* (i: uint32): cfloat {.cdecl, inline.} =
   var v: av_intfloat32
