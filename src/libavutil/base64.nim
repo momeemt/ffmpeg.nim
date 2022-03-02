2392d92fdd82018c0dc61fb8ff3ae0e34554ca82
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 15:49:11 +0900

    👍 base64 to support FFmpeg 5.0

--- src/libavutil/base64.nim
+++ src/libavutil/base64.nim
@@ -1,9 +1,9 @@
 when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll", cdecl.}
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavutil(|.55|.56|.57).dylib", cdecl.}
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)", cdecl.}
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
 
 proc av_base64_decode* (`out`: ptr uint8, `in`: cstring, out_size: cint): cint
 proc av_base64_encode* (`out`: cstring, out_size: cint, `in`: ptr uint8, in_size: cint): cstring
