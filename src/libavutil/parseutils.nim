567c5590bbf11d66983c8dbef9b43e35527da11e
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 19:35:08 +0900

    👍 parseutils to support FFmpeg 5.0

--- src/libavutil/parseutils.nim
+++ src/libavutil/parseutils.nim
@@ -1,12 +1,12 @@
 import std/time_t
-import ffmpeg_types
+from ../types import AVRational, tm
 
 when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll", cdecl.}
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavutil(|.55|.56|.57).dylib", cdecl.}
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)", cdecl.}
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
 
 proc av_parse_ratio* (q: ptr AVRational, str: cstring, max, log_offset: cint, log_ctx: pointer): cint
 proc av_parse_video_size* (width_ptr, height_ptr: ptr cint, str: cstring): cint
