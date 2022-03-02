28983a8adb3b07ec2ae64a0d54a081341dc7e1e3
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 16:58:59 +0900

    👍 hmac to support FFmpeg 5.0

--- src/libavutil/hmac.nim
+++ src/libavutil/hmac.nim
@@ -1,11 +1,11 @@
-import ffmpeg_types
+from ../types import AVHMAC, AVHMACType
 
 when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll", cdecl.}
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavutil(|.55|.56|.57).dylib", cdecl.}
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)", cdecl.}
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
 
 proc av_hmac_alloc* (`type`: AVHMACType): ptr AVHMAC
 proc av_hmac_free* (ctx: ptr AVHMAC)
