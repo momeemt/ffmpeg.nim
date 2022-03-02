b3cdcdf3d397ead62e2b008c3a7070058bdc324d
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 17:57:31 +0900

    👍 spherical to support FFmpeg 5.0

--- src/libavutil/spherical.nim
+++ src/libavutil/spherical.nim
@@ -1,11 +1,11 @@
-import ffmpeg_types
+from ../types import AVSphericalMapping, AVSphericalProjection
 
 when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll", cdecl.}
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavutil(|.55|.56|.57).dylib", cdecl.}
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)", cdecl.}
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
 
 proc av_spherical_alloc* (size: ptr csize_t): ptr AVSphericalMapping
 proc av_spherical_tile_bounds* (map: ptr AVSphericalMapping, width, height: csize_t, left, top, right, bottom: ptr csize_t)
