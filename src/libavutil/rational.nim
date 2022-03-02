332f855af51d515e9af58f397fd04a8458ab08ee
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 18:24:47 +0900

    👍 rational to support FFmpeg 5.0

--- src/libavutil/rational.nim
+++ src/libavutil/rational.nim
@@ -1,11 +1,11 @@
-import ffmpeg_types
+from ../types import AVRational
 
 when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll", cdecl.}
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavutil(|.55|.56|.57).dylib", cdecl.}
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)", cdecl.}
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
 
 proc av_reduce* (dst_num, dst_den: ptr cint, num, denn, max: int64): cint
 proc av_mul_q* (b, c: AVRational): AVRational
