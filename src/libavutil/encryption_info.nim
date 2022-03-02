542fa909afb7bb555212a08c96a533bb085fd515
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 16:39:39 +0900

    👍 encryption_info to support FFmpeg 5.0

--- src/libavutil/encryption_info.nim
+++ src/libavutil/encryption_info.nim
@@ -1,11 +1,11 @@
-import ffmpeg_types
+from ../types import AVEncryptionInfo, AVEncryptionInitInfo
 
 when defined(windows):
-  {.push importc, dynlib: "avcodec(|-55|-56|-57|-58|-59).dll", cdecl.}
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavcodec(|.55|.56|.57|.58|.59).dylib", cdecl.}
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavcodec.so(|.55|.56|.57|.58|.59)", cdecl.}
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
 
 proc av_encryption_info_alloc* (subsample_count, key_id_size, iv_size: cuint): AVEncryptionInfo
 proc av_encryption_info_clone* (info: ptr AVEncryptionInfo): ptr AVEncryptionInfo
