0472b703269d9c43d210164b7105256ed6647200
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 17:39:57 +0900

    👍 timecode to support FFmpeg 5.0

--- src/libavutil/timecode.nim
+++ src/libavutil/timecode.nim
@@ -1,11 +1,11 @@
-import ffmpeg_types
+from ../types import AVTimecode, AVRational
 
 when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll", cdecl.}
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavutil(|.55|.56|.57).dylib", cdecl.}
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)", cdecl.}
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
 
 const
   AV_TIMECODE_STR_SIZE* = 23
@@ -18,3 +18,6 @@ proc av_timecode_make_mpeg_tc_string* (buf: cstring, tc25bit: uint32): cstring
 proc av_timecode_init* (tc: ptr AVTimecode, rate: AVRational, flags, frame_start: cint, log_ctx: pointer): cint
 proc av_timecode_init_from_string* (tc: ptr AVTimecode, rate: AVRational, str: cstring, log_ctx: pointer): cint
 proc av_timecode_check_frame_rate* (rate: AVRational): cint
+proc av_timecode_get_smpte* (rate: AVRational, drop, hh, mm, ss, ff: cint): uint32
+proc av_timecode_make_smpte_tc_string2* (buf: cstring, rate: AVRational, tcsmpte: uint32, prevent_df, skip_field: cint): cstring
+proc av_timecode_init_from_components* (tc: ptr AVTimecode, rate: AVRational, flags, hh, mm, ss, ff: cint, log_ctx: pointer): cint
