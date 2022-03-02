91f5be1910454a18e9516b6016337b7e29ed7a2f
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 12:05:38 +0900

    👍 libavdevice/version to support FFmpeg5.0

--- src/libavdevice/version.nim
+++ src/libavdevice/version.nim
@@ -1,17 +1,18 @@
-from libavutil_version import AV_VERSION_INT, AV_VERSION
+from ../libavutil/version import AV_VERSION_INT, AV_VERSION
 
 when defined(windows):
-  {.push importc, dynlib: "avdevice(|-55|-56|-57|-58).dll", cdecl.}
+  {.push importc, dynlib: "avdevice(|-58|-59|-60|-61|-62).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavdevice(|.55|.56|.57|.58).dylib", cdecl.}
+  {.push importc, dynlib: "libavdevice(|.58|.59|.60|.61|.62).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavdevice.so(|.55|.56|.57|.58)", cdecl.}
+  {.push importc, dynlib: "libavdevice.so(|.58|.59|.60|.61|.62)", cdecl.}
 
 const
-  LIBAVDEVICE_VERSION_MAJOR* = 58
-  LIBAVDEVICE_VERSION_MINOR* = 10
+  LIBAVDEVICE_VERSION_MAJOR* = 59
+  LIBAVDEVICE_VERSION_MINOR* = 4
   LIBAVDEVICE_VERSION_MICRO* = 100
   LIBAVDEVICE_VERSION_INT* = AV_VERSION_INT(LIBAVDEVICE_VERSION_MAJOR, LIBAVDEVICE_VERSION_MINOR, LIBAVDEVICE_VERSION_MICRO)
   LIBAVDEVICE_VERSION* = AV_VERSION(LIBAVDEVICE_VERSION_MAJOR, LIBAVDEVICE_VERSION_MINOR, LIBAVDEVICE_VERSION_MICRO)
   LIBAVDEVICE_BUILD* = LIBAVDEVICE_VERSION_INT
   LIBAVDEVICE_IDENT* = "Lavd"
+  FF_API_DEVICE_CAPABILITIES* = LIBAVDEVICE_VERSION_MAJOR < 60
