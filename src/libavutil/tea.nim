74595f9c5b1cd73855bba06812b89487b1616725
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 17:55:31 +0900

    👍 tea to support FFmpeg 5.0

--- src/libavutil/tea.nim
+++ src/libavutil/tea.nim
@@ -1,11 +1,11 @@
-import ffmpeg_types
+from ../types import AVTEA
 
 when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll", cdecl.}
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavutil(|.55|.56|.57).dylib", cdecl.}
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)", cdecl.}
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
 
 proc av_tea_alloc* (): ptr AVTEA
 proc av_tea_init* (ctx: ptr AVTEA, key: ptr uint8, rounds: cint)
