a34774fd27ec6a0c681ed32c95fcf661aae9f3aa
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 12:05:59 +0900

    👍 avdevice to support FFmpeg5.0

--- src/libavdevice/avdevice.nim
+++ src/libavdevice/avdevice.nim
@@ -1,14 +1,19 @@
-import ffmpeg_types
-from libavutil_dict import AVDictionary
+from ../types import AVOption, AVInputFormat, AVOutputFormat, AVAppToDevMessageType, AVFormatContext, AVDeviceCapabilitiesQuery, AVDeviceInfoList, AVDevToAppMessageType
+from ../libavutil/dict import AVDictionary
+from version import FF_API_DEVICE_CAPABILITIES
 
 when defined(windows):
-  {.push importc, dynlib: "avdevice(|-55|-56|-57|-58).dll", cdecl.}
+  {.push importc, dynlib: "avdevice(|-58|-59|-60|-61|-62).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavdevice(|.55|.56|.57|.58).dylib", cdecl.}
+  {.push importc, dynlib: "libavdevice(|.58|.59|.60|.61|.62).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavdevice.so(|.55|.56|.57|.58)", cdecl.}
+  {.push importc, dynlib: "libavdevice.so(|.58|.59|.60|.61|.62)", cdecl.}
 
-var av_device_capabilities*: ptr AVOption
+when FF_API_DEVICE_CAPABILITIES:
+  var av_device_capabilities* {.deprecated.} : ptr AVOption
+  
+  proc avdevice_capabilities_create* (caps: ptr ptr AVDeviceCapabilitiesQuery, s: ptr AVFormatContext, device_options: ptr ptr AVDictionary): cint {.deprecated.}
+  proc avdevice_capabilities_free* (caps: ptr ptr AVDeviceCapabilitiesQuery, s: ptr AVFormatContext) {.deprecated.}
 
 proc avdevice_version* (): cuint
 proc avdevice_configuration* (): cstring
@@ -20,8 +25,6 @@ proc av_output_audio_device_next* (d: ptr AVOutputFormat): ptr AVOutputFormat
 proc av_output_video_device_next* (d: ptr AVOutputFormat): ptr AVOutputFormat
 proc avdevice_app_to_dev_control_message* (s: ptr AVFormatContext, `type`: AVAppToDevMessageType, data: pointer, data_size: csize_t): cint
 proc avdevice_dev_to_app_control_message* (s: ptr AVFormatContext, `type`: AVDevToAppMessageType, data: pointer, data_size: csize_t): cint
-proc avdevice_capabilities_create* (caps: ptr ptr AVDeviceCapabilitiesQuery, s: ptr AVFormatContext, device_options: ptr ptr AVDictionary): cint
-proc avdevice_capabilities_free* (caps: ptr ptr AVDeviceCapabilitiesQuery, s: ptr AVFormatContext)
 proc avdevice_list_devices* (s: ptr AVFormatContext, device_list: ptr ptr AVDeviceInfoList): cint
 proc avdevice_free_list_devices* (device_list: ptr ptr AVDeviceInfoList)
 proc avdevice_list_input_sources* (device: ptr AVInputFormat, device_name: cstring, device_options: ptr AVDictionary, device_list: ptr ptr AVDeviceInfoList): cint
