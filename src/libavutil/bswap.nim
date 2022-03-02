d0a6f8cab315f60b0128588db84d21ea18d36d55
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 15:51:58 +0900

    👍 bswap to support FFmpeg 5.0

--- src/libavutil/bswap.nim
+++ src/libavutil/bswap.nim
@@ -1,9 +1,9 @@
 when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll", cdecl.}
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavutil(|.55|.56|.57).dylib", cdecl.}
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)", cdecl.}
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
 
 # when defined(HAVE_AV_CONFIG_H):
   #if   ARCH_AARCH64
