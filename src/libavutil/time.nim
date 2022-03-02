28ce1c21d5e1be69d0282d99a67822a431da54d7
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 17:40:44 +0900

    👍 time to support FFmpeg 5.0

---
+++ src/libavutil/time.nim
@@ -0,0 +1,11 @@
+when defined(windows):
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
+elif defined(macosx):
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
+else:
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
+
+proc av_gettime* (): int64
+proc av_gettime_relative* (): int64
+proc av_gettime_relative_is_monotonic* (): cint
+proc av_usleep* (usec: cuint): cint
