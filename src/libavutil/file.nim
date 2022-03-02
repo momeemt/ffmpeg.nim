f13b0476e394364199c9a40d7970b182e3cd868e
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 16:45:00 +0900

    👍 file to support FFmpeg 5.0

--- src/libavutil/file.nim
+++ src/libavutil/file.nim
@@ -1,9 +1,9 @@
 when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll", cdecl.}
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavutil(|.55|.56|.57).dylib", cdecl.}
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)", cdecl.}
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
 
 proc av_file_map* (filename: cstring, bufptr: ptr ptr uint8, size: ptr csize_t, log_offset: cint, log_ctx: pointer): cint # warning
 proc av_file_unmap* (bufptr: ptr uint8, size: csize_t)
