bd90f47e98917e19b3b245abddd72f0022b7b2d7
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 19:50:40 +0900

    👍 lfg to support FFmpeg 5.0

--- src/libavutil/lfg.nim
+++ src/libavutil/lfg.nim
@@ -1,11 +1,11 @@
-import ffmpeg_types
+from ../types import AVLFG
 
 when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll", cdecl.}
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavutil(|.55|.56|.57).dylib", cdecl.}
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)", cdecl.}
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
 
 proc av_lfg_init* (c: ptr AVLFG, seed: cuint)
 proc av_lfg_init_from_data* (c: ptr AVLFG, data: ptr uint8, length: cuint): cint
