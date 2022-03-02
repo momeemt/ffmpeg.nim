4eafdb580784dbb5df0c63a7222eff19726f2f43
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 16:42:18 +0900

    👍 eval to support FFmpeg 5.0

--- src/libavutil/eval.nim
+++ src/libavutil/eval.nim
@@ -1,11 +1,11 @@
-import ffmpeg_types
+from ../types import AVExpr
 
 when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll", cdecl.}
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavutil(|.55|.56|.57).dylib", cdecl.}
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)", cdecl.}
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
 
 proc av_expr_parse_and_eval* (res: ptr cdouble, s: cstring, const_names: cstringArray, const_values: ptr cdouble, func1_names: cstringArray, funcs1: proc (a1: pointer, a2: cdouble): cdouble, func2_names: cstringArray, funcs2: proc (a1: pointer, a2: cdouble, a3: cdouble): cdouble, opaque: pointer, log_offset: cint, log_ctx: pointer): cint
 proc av_expr_parse* (expr: ptr ptr AVExpr, s: cstring, const_names: cstringArray, const_values: ptr cdouble, func1_names: cstringArray, funcs1: proc (a1: pointer, a2: cdouble): cdouble, func2_names: cstringArray, funcs2: proc (a1: pointer, a2: cdouble, a3: cdouble): cdouble, opaque: pointer, log_offset: cint, log_ctx: pointer): cint
