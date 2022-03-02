ac1219ad755c336158d8eeb90c5d300cb1fbf78e
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 17:56:33 +0900

    👍 stereo3d to support FFmpeg 5.0

--- src/libavutil/stereo3d.nim
+++ src/libavutil/stereo3d.nim
@@ -1,11 +1,11 @@
-import ffmpeg_types
+from ../types import AVFrame, AVStereo3D
 
 when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll", cdecl.}
+  {.push importc, dynlib: "avutil-(|56|57|58|59|60).dll", cdecl.}
 elif defined(macosx):
-  {.push importc, dynlib: "libavutil(|.55|.56|.57).dylib", cdecl.}
+  {.push importc, dynlib: "libavutil(|.56|.57|.58|.59|.60).dylib", cdecl.}
 else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)", cdecl.}
+  {.push importc, dynlib: "libavutil.so(|.56|.57|.58|.59|.60)", cdecl.}
 
 const
   AV_STEREO3D_FLAG_INVERT* = 1 shl 0
