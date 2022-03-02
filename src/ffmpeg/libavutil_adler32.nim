be339163dc4f737af2f8b9b041ea5708c0f95be4
Mutsuha Asada <momiimt@gmail.com>
2022-03-02 15:20:20 +0900

    👍 adler32 to support FFmpeg 5.0

--- src/ffmpeg/libavutil_adler32.nim
+++
@@ -1,8 +0,0 @@
-when defined(windows):
-  {.push importc, dynlib: "avutil-(|55|56|57).dll", cdecl.}
-elif defined(macosx):
-  {.push importc, dynlib: "libavutil(|.55|.56|.57).dylib", cdecl.}
-else:
-  {.push importc, dynlib: "libavutil.so(|.55|.56|.57)", cdecl.}
-
-proc av_adler32_update* (adler: culong, buf: ptr uint8, len: cuint): culong
